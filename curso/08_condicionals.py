f065fc9eb5c705e1b87934bc77c975a63cc98729
Jose Marcelo <jamarcelor07@gmail.com>
2026-04-09 11:56:38 -0500

    teoria del condicional if

---
+++ curso/08_condicionals.py
@@ -0,0 +1,27 @@
+my_condition = False
+
+if my_condition:
+    print("Se ejecuta la condicion del if")
+
+my_condition = 5 * 2
+
+if my_condition == 10:
+    print("Se ejecuta la condicion del segundo if")
+
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 1:
+    print("Es igual a 1")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
+
+
+my_string = ""
+
+if not my_string: # -> se cumple si el string esta vacio
+    print("Mi cadena de texto esta vacia")
+
+if my_string == "Mi cadena de texto":
+    print("Estas cadenas coinciden")
+
+print("La ejecucion continua")
